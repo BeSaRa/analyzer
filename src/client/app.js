e619ee4c6425efb13f8c7563a9d45e2bb6500478
BeSaRa <besara@windowslive.com>
2022-01-03 23:41:38 +0300

    Create App.js file as main entry point for webpack

---
+++ src/client/app.js
@@ -0,0 +1,12 @@
+import "bootstrap/scss/bootstrap.scss"
+
+export function handleSubmit(e) {
+    console.log(e);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const analyseBtn = document.getElementById('analyseBtn');
+    const urlInput = document.getElementById('urlInput');
+})
+
+
